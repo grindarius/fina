ea876e22b6156439df239f508c42f5886f6ea049
Bhattarapong Somwong <numbbutt34685@gmail.com>
2021-05-23 23:40:21 +0700

    convert flask to flask-restful

--- functions/index.py
+++ functions/index.py
@@ -1,10 +1,14 @@
-import flask
+from flask import Flask
+from flask_restful import Resource, Api
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
+api = Api(app)
 
-@app.route('/', methods = ['GET'])
-def home() -> str:
-  return '<h1> Fuckerssss </h1>'
+class hello_world(Resource):
+  def get(self):
+    return { 'hello': 'world' }
+
+api.add_resource(hello_world, '/')
 
 if __name__ == '__main__':
-  app.run(debug = True)
+  app.run(debug=True)
--- src/views/globals/home/home.ts
+++ src/views/globals/home/home.ts
@@ -9,6 +9,7 @@ import { Vue, Component } from 'vue-property-decorator'
 })
 export default class Home extends Vue {
   private readonly expression = 'x^3 - x - 1'
+
   private readonly points: Array<Coordinate> = [
     {
       x: 1.5,
