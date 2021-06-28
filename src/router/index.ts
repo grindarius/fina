44a22ec39a7b0cf89a22ddf1d49e371e02ee4fd6
Bhattarapong Somwong <numbbutt34685@gmail.com>
2021-06-28 15:31:24 +0700

    adds scroll behavior for the page

--- src/router/index.ts
+++ src/router/index.ts
@@ -56,7 +56,14 @@ const routes: Array<RouteConfig> = [
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes,
+  scrollBehavior (to) {
+    if (to.hash != null && to.hash !== '') {
+      return { selector: to.hash }
+    } else {
+      return { x: 0, y: 0 }
+    }
+  }
 })
 
 export default router
