7d631b1209432d524097de0e988f599cba0ed2e3
Bhattarapong Somwong <numbbutt34685@gmail.com>
2021-07-27 08:45:05 +0700

    Functions: adds preValidaiton hook

--- functions/src/routes/bisection/index.ts
+++ functions/src/routes/bisection/index.ts
@@ -1,6 +1,6 @@
 import { FastifyInstance, FastifyPluginOptions, FastifySchema } from 'fastify'
 
-import { Answer, BisectionQuerystring, BisectionQuerystringSchema, BisectionResponseSchema } from '@fina/common'
+import { Answer, BisectionQuerystring, BisectionQuerystringSchema, BisectionResponseSchema, evaluateFunction, round } from '@fina/common'
 
 import { bisectionIteration } from '../../services/bisection'
 
@@ -12,7 +12,36 @@ const schema: FastifySchema = {
 }
 
 export default async function (instance: FastifyInstance, _: FastifyPluginOptions): Promise<void> {
-  instance.get<{ Querystring: BisectionQuerystring }>('/', { schema }, async (request, _) => {
+  instance.get<{ Querystring: BisectionQuerystring }>('/', {
+    schema,
+    preValidation: [
+      async (request, reply, done) => {
+        if (request.query.start > request.query.end) {
+          await reply.code(400).send({
+            statusCode: 400,
+            error: 'Bad request',
+            message: 'Property \'start\' is greater than \'end\''
+          })
+        } else {
+          done()
+        }
+      },
+      async (request, reply, done) => {
+        const fa = round(evaluateFunction(request.query.expression, request.query.start), request.query.dp ?? 5)
+        const fb = round(evaluateFunction(request.query.expression, request.query.start), request.query.dp ?? 5)
+
+        if (fa * fb >= 0) {
+          await reply.code(400).send({
+            statusCode: 400,
+            error: 'Bad request',
+            message: 'f(a) and f(b) does not have opposing signs'
+          })
+        } else {
+          done()
+        }
+      }
+    ]
+  }, async (request, _) => {
     const expression = request.query.expression
     const a = request.query.start
     const b = request.query.end
