ba00c1363365118661f2740a3a3986872ab6030a
Bhattarapong Somwong <numbbutt34685@gmail.com>
2021-07-29 14:51:59 +0700

    Functions: Update return types for sd endpoints

--- functions/src/services/secant.ts
+++ functions/src/services/secant.ts
@@ -1,25 +1,25 @@
-import { absoluteError, Answer, evaluateFunction, round } from '@fina/common'
+import { absoluteError, evaluateFunction, round, SecantResponse, SecantResponseObject } from '@fina/common'
 
-export function secantIteration (expression: string, a: number, b: number, iteration: number, decimalPoint: number): Array<Answer> {
+export function secantIteration (expression: string, a: number, b: number, iteration: number, decimalPoint: number): SecantResponse {
   let previousC = 0
-  const answerArray: Array<Answer> = []
+  const answerArray: SecantResponse = []
 
   for (let i = 0; i < iteration; i++) {
     // * find f(a), f(b)
-    const fa = round(evaluateFunction(expression, a), decimalPoint)
-    const fb = round(evaluateFunction(expression, b), decimalPoint)
+    const fa = round(evaluateFunction(expression, { x: a }), decimalPoint)
+    const fb = round(evaluateFunction(expression, { x: b }), decimalPoint)
 
     // * find c
     const cExpression = ((a * fb) - (b * fa)) / (fb - fa)
     const c = round(cExpression, decimalPoint)
 
     // * find f(c)
-    const fc = round(evaluateFunction(expression, c), decimalPoint)
+    const fc = round(evaluateFunction(expression, { x: c }), decimalPoint)
 
     // * find error
     const error = round(absoluteError(c, previousC), decimalPoint)
 
-    const answer: Answer = { i, a, b, fa, fb, c, fc, error }
+    const answer: SecantResponseObject = { i, a, b, fa, fb, c, fc, error }
     answerArray.push(answer)
 
     // * a becomes b, b becomes c
--- functions/src/services/significant-digits.ts
+++ functions/src/services/significant-digits.ts
@@ -1,18 +1,32 @@
-export function findSignificantDigits (input: string): string {
+import { SignificantDigitsResponse } from '@fina/common'
+
+export function findSignificantDigits (input: string): SignificantDigitsResponse {
   const leadingZerosRegExp: RegExp = /^0+\.?0*/g
   const trailingZerosRegExp: RegExp = /0+$/g
 
   if (input.includes('.')) {
     if (parseFloat(input) > 0) {
       const removedLeadingZeros: string = input.replace(leadingZerosRegExp, '')
-      return removedLeadingZeros
+      return {
+        output: removedLeadingZeros,
+        amount: removedLeadingZeros.length
+      }
     } else if (parseFloat(input) === 0) {
-      return '0'
+      return {
+        output: '0',
+        amount: 1
+      }
     } else {
-      return input
+      return {
+        output: input,
+        amount: input.length
+      }
     }
   } else {
     const removedTrailingZeros: string = input.replace(trailingZerosRegExp, '')
-    return removedTrailingZeros
+    return {
+      output: removedTrailingZeros,
+      amount: removedTrailingZeros.length
+    }
   }
 }
