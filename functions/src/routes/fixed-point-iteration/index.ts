b4bfee87afb3acd1730c1b2cbabaaae0aa57c980
Bhattarapong Somwong <numbbutt34685@gmail.com>
2021-07-30 17:16:47 +0700

    Functions: Fixed Point Iteration Endpoint

---
+++ functions/src/routes/fixed-point-iteration/index.ts
@@ -0,0 +1,42 @@
+import { FastifyInstance, FastifyPluginOptions, FastifySchema } from 'fastify'
+
+import { deriveFunction, FixedPointIterationQuerystring, FixedPointIterationQuerystringSchema, FixedPointIterationResponseSchema, round } from '@fina/common'
+
+import { fixedPointIterationIteration } from '../../services'
+
+const schema: FastifySchema = {
+  querystring: FixedPointIterationQuerystringSchema,
+  response: {
+    200: FixedPointIterationResponseSchema
+  }
+}
+
+export default async function (instance: FastifyInstance, _: FastifyPluginOptions): Promise<void> {
+  instance.get<{ Querystring: FixedPointIterationQuerystring }>('/', {
+    schema,
+    preValidation: async (request, reply, done) => {
+      console.log(request.query)
+      const gpa = round(deriveFunction(request.query.fixedExpression, 'x').evaluate({ x: request.query.start }), request.query.dp ?? 5)
+
+      if (Math.abs(gpa) > 1) {
+        await reply.code(400).send({
+          statusCode: 400,
+          error: 'Bad request',
+          message: `fixed expression does not converge since |g'(x)| = ${gpa}`
+        })
+      } else {
+        done()
+      }
+    }
+  }, async (request, _) => {
+    const expression = request.query.expression
+    const fixedExpression = request.query.fixedExpression
+    const a = request.query.start
+    const iteration = request.query.iteration ?? 5
+    const dp = request.query.dp ?? 5
+
+    const answer = fixedPointIterationIteration(expression, fixedExpression, a, iteration, dp)
+
+    return answer
+  })
+}
